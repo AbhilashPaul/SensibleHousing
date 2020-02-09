ca8d0f82164eff1e7f82defd18fdc4fb1bf4efae
Kenji-Jumalon <51454451+Kenji-Jumalon@users.noreply.github.com>
2020-02-09 12:21:54 -0700

    Dropdownlist added

--- react-mortgagecalc/src/components/CalculateForm.js
+++ react-mortgagecalc/src/components/CalculateForm.js
@@ -62,23 +62,52 @@ class CalculateForm extends Component {
                         <label>{this.state.interest}</label>
                     </div>
                     <div>
-                        <input></input>
+                        <select> 
+                            <option >0.5 year</option>
+                            <option >2 years</option>
+                        </select>
                     </div>
                     <div>
                         <label>{this.state.amortization}</label>
                     </div>
                     <div>
-                        <input></input>
+                        <select>
+                            <option >0.5 year</option>
+                            <option >2 years</option>
+                            <option >3 years</option>
+                            <option >4 years</option>
+                            <option >5 years</option>
+                            <option >6 years</option>
+                            <option >7 years</option>
+                            <option >8 years</option>
+                            <option >9 years</option>
+                            <option >10 years</option>
+                            <option >11 years</option>
+                            <option >12 years</option>
+                            <option >13 years</option>
+                            <option >14 years</option>
+                            <option >15 years</option>
+                            <option >16 years</option>
+                            <option >17 years</option>
+                            <option >18 years</option>
+                            <option >19 years</option>
+                            <option >20 years</option>
+                            <option >21 years</option>
+                            <option >22 years</option>
+                            <option >23 years</option>
+                            <option >24 years</option>
+                            <option >25years</option>
+                        </select>
                     </div>
                     <div>
                         <label>{this.state.payment}</label>
                     </div>
                     <div>
-                    <select>
-                        <option value="grapefruit">Weekly</option>
-                        <option value="lime">Bi-Weekly</option>
-                        <option selected value="coconut">Monthly</option>
-                    </select>
+                        <select>
+                            <option >Weekly</option>
+                            <option >Bi-Weekly</option>
+                            <option >Monthly</option>
+                        </select>
                     </div>
                     <div>
                         <button>Calculate my payments</button>
