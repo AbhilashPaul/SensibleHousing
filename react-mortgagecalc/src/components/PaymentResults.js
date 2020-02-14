005bb60f35a5952061428d45df8137eb6be60afe
Kenji-Jumalon <51454451+Kenji-Jumalon@users.noreply.github.com>
2020-02-13 23:04:07 -0700

    PaymentResults.js layout added

--- react-mortgagecalc/src/components/PaymentResults.js
+++ react-mortgagecalc/src/components/PaymentResults.js
@@ -1,2 +1,26 @@
 import React, { Component } from "react"
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
+
+function PaymentResults() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={6}>2 of 3 (wider)</Col>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+            </Container>
+            
+        </div>
+    )
+}
+
+export default PaymentResults
